c0fc07a2795665ebcefbdfd5c8859866f6b916ad
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-01-20 01:54:43 -0800

    Create hello10.c

---
+++ hello10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int n,num,p;
+scanf("%d",&n);
+while(n>0)
+{
+scanf("%d %d",&num,p);
+power=pow(num,p);
+printf("%d",power);
+n--;
+}
+getch;
+}
